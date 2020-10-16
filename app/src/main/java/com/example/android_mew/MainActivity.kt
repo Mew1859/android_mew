da425be6bebb5caac032d9f5adf46975bf3d9b34
Mew1859 <68700706+Mew1859@users.noreply.github.com>
2020-10-16 11:09:57 +0700

    16/10/63
    16/10/2020

--- app/src/main/java/com/example/android_mew/MainActivity.kt
+++ app/src/main/java/com/example/android_mew/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.android_mew
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        login.setOnClickListener {
+            val username = username_login.text.toString()
+            val password = password_login.text.toString()
+
+            if (username == null || password == null){
+                Toast.makeText(applicationContext, "$username,$password,null", Toast.LENGTH_SHORT).show()
+            }
+            if (username == "g@gmail.com" && password == "1234"){
+                Toast.makeText(applicationContext, "$username,$password,successful", Toast.LENGTH_SHORT).show()
+                val intent = Intent(applicationContext,page2::class.java)
+                startActivity(intent)
+                finish()
+            }
+            Toast.makeText(applicationContext, "$username, $password", Toast.LENGTH_SHORT).show()
+        }
     }
 }
